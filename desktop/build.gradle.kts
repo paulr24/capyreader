7cacda832d73d09a75fb38961da4181a89af7850
paulr24 <paulr315@gmail.com>
2026-09-18 18:29:51 -0400

    Bump desktop packageVersion to 1.0.1246

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -42,7 +42,7 @@ compose.desktop {
                 org.jetbrains.compose.desktop.application.dsl.TargetFormat.Exe
             )
             packageName = "CapyReader"
-            packageVersion = "1.0.1245"
+            packageVersion = "1.0.1246"
             description = "Capy Reader - RSS Reader for Desktop"
             copyright = "© 2026 Capy Reader"
             vendor = "Capy Reader"
